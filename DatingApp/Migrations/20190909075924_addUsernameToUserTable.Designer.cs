2c506844be4a42bb18feff34df5f19fa03526969
hatef khodkar <hatef.khodkar@hotmail.com>
2019-09-09 12:41:23 +0430

    Add Username field to User table

--- DatingApp/Migrations/20190909075924_addUsernameToUserTable.Designer.cs
+++ DatingApp/Migrations/20190909075924_addUsernameToUserTable.Designer.cs
@@ -4,14 +4,16 @@ using DatingApp.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190909075924_addUsernameToUserTable")]
+    partial class addUsernameToUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DatingApp/Models/User.cs
+++ DatingApp/Models/User.cs
@@ -8,6 +8,7 @@ namespace DatingApp.Models
     public class User
     {
         public int Id { get; set; }
+        public string Username { get; set; }
         public byte[] Password { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
